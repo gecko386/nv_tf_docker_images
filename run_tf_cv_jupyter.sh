8f947ec7684273d93c43938f9c9bbd33695b79c9
Rubén Abad <gecko386@gmail.com>
2020-11-16 23:20:13 +0100

    Use $HOME as root folder for jupyter
    the container does not start jupyter notebook anymore. it's just now launched
    from the script, passing the --notebook-dir=$HOME option

--- run_tf_cv_jupyter.sh
+++ run_tf_cv_jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=6710886 --gpus all --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -p 8888:8888 nv_tf_cv_jupyter
+docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=6710886 --gpus all -w $HOME --mount src=/home,target=/home,type=bind --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -p 8888:8888 nv_tf_cv_jupyter jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root --notebook-dir=$HOME
