2f6cd8656bb9646e79d314d8c39de90953dbe337
Rubén Abad <gecko386@gmail.com>
2021-01-10 03:01:10 +0100

    folder structure reorganized (2)

--- run_tf_cv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xhost +
-
-docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=6710886 --gpus all -it  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w $HOME --mount src=/home,target=/home,type=bind --cap-add=SYS_PTRACE --security-opt seccomp=unconfined nv_tf_cv
